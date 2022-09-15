4fa55513c9a8559211f16b960be293881ce16920
sleek1004 <arojojoyeo@yahoo.com>
2022-09-15 21:34:21 +0100

    building emoji fighter with HTML,CSS & JavaScript

---
+++ index.js
@@ -0,0 +1,8 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+
+   let randomIndex = Math.random()
+})
